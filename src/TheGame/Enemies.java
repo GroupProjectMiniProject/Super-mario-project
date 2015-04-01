59da8539c882bb3148942b84195f725ee0f6fde6
Slashpwn <sonn.slash@gmail.com>
2015-04-01 11:59:37 +0200

    We will from now on continue to build on Ondrejs package, but we changed the name. The TestOndrej package can be used locally for experimental purposes while TheGame package are for final designs

---
+++ src/TheGame/Enemies.java
@@ -0,0 +1,13 @@
+package TheGame;
+
+import org.newdawn.slick.Image;
+
+public class Enemies {
+	public int x, y, speedMax, health;
+	public float speedX, speedY;
+
+	public void draw(Image image) {
+		image.draw(x, y);
+		
+	}
+}
