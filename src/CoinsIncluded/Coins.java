d9a63289ad82038fd52bd43870e25f6910dd918a
Tolga-Tasbent <ttasbe13@student.aau.dk>
2015-04-15 11:25:37 +0200

    COINS

---
+++ src/CoinsIncluded/Coins.java
@@ -0,0 +1,15 @@
+package CoinsIncluded;
+
+import org.newdawn.slick.Image;
+
+public class Coins extends GameState {
+	
+	public static int Cx=400;
+	public static int Cy=400;
+
+	public void draw(Image image) {
+		image.draw(Cx, Cy);
+		System.out.println("COIN DRAWN");
+	}
+	
+}
