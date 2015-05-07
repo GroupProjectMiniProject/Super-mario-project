a62609c6ef1af3a15e474b9f609fb3d87fe038b7
Tolga-Tasbent <ttasbe13@student.aau.dk>
2015-05-07 14:53:17 +0200

    loadingplatforms

--- src/EnemiesIncluded/LoadingPlatforms.java
+++ src/EnemiesIncluded/LoadingPlatforms.java
@@ -4,14 +4,62 @@ import org.newdawn.slick.geom.Rectangle;
 
 public class LoadingPlatforms extends GameState {
 	public LoadingPlatforms (){
-		
 	}
 	public static void start(){
 		for (int i=0; i<X; i+=texSize){
 			platforms.add(platform_basic);
 			platformsShapes.add(new Rectangle(i, bottom, texSize, texSize));
-		
 		}
+		
+		flagShape = new Rectangle(X-texSize, bottom-texSize, texSize, texSize);
+		
+		//MADE THIS
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (400, 400, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (X-texSize, 400, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (680, 350, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (650, 300, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (600, 250, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (400, 250, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (400-texSize, 250, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (150, texSize*6, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (150-texSize, texSize*6, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (150-texSize*2, texSize*6, texSize, texSize));
+		
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (texSize*6, 300-texSize, texSize, texSize));
+		
+		for (int i=0; i<150; i+=texSize){
+		platforms.add(platform_basic);
+		platformsShapes.add(new Rectangle (X-i, bottom-4*texSize, texSize, texSize));
+		}
+		
+		for (int i=0; i<200; i+=texSize){
+			platforms.add(platform_basic);
+			platformsShapes.add(new Rectangle(i, 300, texSize, texSize));
+		}
+		//MADE THE STUFF ABOVE
+	
+		
 		platforms.add(platform_basic);
 		platformsShapes.add(new Rectangle (200, 400, texSize, texSize));
 		
@@ -23,9 +71,6 @@ public class LoadingPlatforms extends GameState {
 		platforms.add(platform_basic);
 		
 		platformsShapes.add(new Rectangle (200-3*texSize, 400+3*texSize, texSize, texSize));
-		
-		platforms.add(platform_basic);
-		platformsShapes.add(new Rectangle (X-texSize*4, bottom-4*texSize, texSize, texSize));
 		
 		platforms.add(platform_basic);
 		platformsShapes.add(new Rectangle (0, bottom-2*texSize, texSize, texSize));
