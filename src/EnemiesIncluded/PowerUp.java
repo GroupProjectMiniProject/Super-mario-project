ceebac54788fa612e7298317f56f84e474b05f0a
Slashpwn <sonn.slash@gmail.com>
2015-05-07 19:37:14 +0200

    Coin conter fix PowerUp placement and commenting

--- src/EnemiesIncluded/PowerUp.java
+++ src/EnemiesIncluded/PowerUp.java
@@ -14,22 +14,24 @@ public class PowerUp extends GameState {
 
 	public static void start (){
 		
-		powerUpList.clear(); 
+		//Clear the array for re setting the stage
+		powerUpList.clear();  
 		
-		for (int i = 0; i < 2; i++){
-			//powerUpTexList.add(powerUpTex);
-			powerUpList.add(new Rectangle(200+(i*400), 550, powerUpTex.getWidth(), powerUpTex.getHeight()));
+		
+		//Creating new elements to Rectangle array defined in GameState. It gets the width and height of the texture for powerUp
+		powerUpList.add(new Rectangle(395, 235, powerUpTex.getWidth(), powerUpTex.getHeight())); 
+		powerUpList.add(new Rectangle(10, 550-2*texSize, powerUpTex.getWidth(), powerUpTex.getHeight()));
 
-			
-		}
 	
 	}	
+	
 	public static void interaction () {
-		
+		//Starting a for loop which goes backwards from the array list. So we can delete from the end of the list.
 		for (int i = powerUpList.size()-1; i>= 0; i-- )
 		{
 			Rectangle e = powerUpList.get(i);
 			
+			//If mario's boundingbox is intersecting with an element of the rectangle array remove the current element
 			if (marioShape.intersects(e)){
 				poweredUp = true; 
 				powerUpList.remove(i);
